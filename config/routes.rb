ec4bd86652b5923285733000bfe6934a25e541b0
mitsusada1024 <kouki06181024@gmail.com>
2024-02-03 16:03:08 +0900

    reset_password

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'password_resets/new'
+  get 'password_resets/edit'
   get 'sessions/new'
   get 'users/new', to: 'users#new', as: 'createacount'
   root 'static_pages#home'
@@ -12,4 +14,5 @@ Rails.application.routes.draw do
   resources :main
   resources :users
   resources :account_activations, only: [:edit]
+  resources :password_resets,     only: [:new, :create, :edit, :update]
 end
